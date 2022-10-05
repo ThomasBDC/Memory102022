20697a058cedaf6acab2872edfea66211d411903
Thomas BUREAU DU COLOMBIER <sio.bureauducolombier@gmail.com>
2022-10-05 19:12:26 +0200

    Ajout début et fin de partie, choix nbr de cartes

--- scripts.js
+++ scripts.js
@@ -1,12 +1,7 @@
 /*
 TODO LIST
 
-- Gérer l'aléatoire dans le placement des images
-- Pouvoir choisir le nombre de cartes en jeu
-- Gérer les débuts et fin de partie
 - Ajouter une animation (confeti) lors de victoire 
-- Pouvoir relancer une partie
-- Gérer le bug du click deux fois sur une même image
 - Compter le nombre de coûts pour gagner (stocker des stats en cookie ?)
 
 */
@@ -14,17 +9,28 @@ TODO LIST
 
 //déclarer un tableau de toutes les cartes
 let jeuTableau;
-let allCards = document.querySelectorAll(".card");
-let cptClickCurrent = 0;
-let dataImageShowed;
 
-allCards.forEach(card => {
-    card.addEventListener("click", function(){
-        playGame(card);  
-    });
+let cptClickCurrent = 0;
+let CardClickedId;
+const cards = ["king", "queen", "valet", "as", "kingpiq", "kingtrefle"];
+const gameBoard = document.getElementById("GameBoard");
+let nbPairesOnGame;
+let cptCartesTrouvees = 0;
+
+document.getElementById("playButton").addEventListener("click", function(){
+    let nbCardInput = document.getElementById("nbCardInput");
+    initGame(nbCardInput.value);
 });
 
-function playGame(card){
+/*
+Cette fonction gère ce qui se passe
+quand on clique sur une carte
+*/
+function clickOnCardEvent(card){
+    let allCards = document.querySelectorAll(".card");
+    if(card.classList.contains("finded")){
+        return;
+    }
     cptClickCurrent ++;
     
     if(cptClickCurrent == 1){
@@ -41,26 +47,85 @@ function playGame(card){
         //j'affiche la carte que je viens de cliquer 
         card.classList.remove("hidden");
         //je stocke la réponse derrière la carte
-        dataImageShowed = card.dataset.image;
+        CardClickedId = card.id;
     }
     else if(cptClickCurrent == 2){
         //deuxième click, je vérifie si l'image a été trouvée
-        card.classList.remove("hidden");
-        if(dataImageShowed == card.dataset.image){
-            allCards.forEach(card => {
-                if(card.classList.contains("hidden")){
-                    //c'est une carte cachée
-                }
-                else{
-                    card.classList.add("finded");
-                }
-            });
+        if(CardClickedId == card.id){
+            cptClickCurrent = 1;
+            return;
         }
+        else{
+            card.classList.remove("hidden");
+            let cardClickedBefore = document.getElementById(CardClickedId);
+            if(cardClickedBefore.dataset.image == card.dataset.image){
+                allCards.forEach(card => {
+                    if(card.classList.contains("hidden")){
+                        //c'est une carte cachée
+                    }
+                    else if(!card.classList.contains("finded")){
+                        card.classList.add("finded");
+                        cptCartesTrouvees++;
+                    }
+                });
+            }
+    
+            cptClickCurrent = 0;
+            CardClickedId = "";
 
-        cptClickCurrent = 0;
-        dataImageShowed = "";
-
-        //Compter les cards qui n'ont pas la classe 'finded'
-        //si = 0 alors on a gagné, le jeu est fini
+            if(cptCartesTrouvees == nbPairesOnGame*2){
+                //Animation rigolote
+                alert("gagné");
+            }
+        }
     }
 }
+
+function initGame(nbPaires){
+    gameBoard.innerHTML = "";
+    nbPairesOnGame = nbPaires;
+    cptCartesTrouvees = 0;
+    let gameCard = [];
+    for (let i = 0; i < nbPaires; i++) {
+        gameCard.push([cards[i], false]);
+        gameCard.push([cards[i], false]);
+    }
+    console.log(gameCard);
+
+    for(let i = 0; i < gameCard.length; i++){
+        let cardIsPositionned = false;
+        while(!cardIsPositionned){
+            let randomNumber = getRandomArbitrary(0, gameCard.length);
+            if(gameCard[randomNumber][1] == false){
+                cardIsPositionned = true;
+                gameCard[randomNumber][1] = true;
+                //Faut générer le code HTML, et l'inclure.
+                let cardHtml = getHtmlCodeCard(gameCard[randomNumber][0], i);
+                gameBoard.innerHTML += cardHtml;
+            }
+        }
+        
+    }
+
+    //Ajouter l'évènement du click
+    /*
+    J'ajoute l'évènement de click sur toutes les cartes
+    */
+    let allCards = document.querySelectorAll(".card");
+    allCards.forEach(card => {
+        card.addEventListener("click", function(){
+            clickOnCardEvent(card);  
+        });
+    });
+}
+
+function getRandomArbitrary(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function getHtmlCodeCard(nomCard, id){
+    return `<div class="card hidden" id="${id}" data-image="${nomCard}">
+                <img src="/img/${nomCard}.png" />
+            </div>`;
+}
+
