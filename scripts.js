734a0972124fb4db4752017f497c9dcfa2aab986
Thomas BUREAU DU COLOMBIER <sio.bureauducolombier@gmail.com>
2022-10-19 18:54:07 +0200

    Set best score and fix online img

--- scripts.js
+++ scripts.js
@@ -15,6 +15,13 @@ const cards = ["king", "queen", "valet", "as", "kingpiq", "kingtrefle"];
 const gameBoard = document.getElementById("GameBoard");
 let nbPairesOnGame;
 let cptCartesTrouvees = 0;
+const nbCoupsCurrentNode = document.getElementById("NbCoupsCurrent");
+const BestScoreNode = document.getElementById("BestScore");
+const BestScoreCookie ="BestScore";
+
+let nbCoups = 0;
+
+BestScoreNode.innerText = getCookie(BestScoreCookie)
 
 document.getElementById("playButton").addEventListener("click", function(){
     let nbCardInput = document.getElementById("nbCardInput");
@@ -84,12 +91,21 @@ function clickOnCardEvent(card){
                 });
             }
     
+            nbCoups++;
+            nbCoupsCurrentNode.innerText = nbCoups;
             cptClickCurrent = 0;
             CardClickedId = "";
 
             if(cptCartesTrouvees == nbPairesOnGame*2){
                 //Animation rigolote
                 setAnimationWin();
+
+                //Partie terminée, je mets à jour les cookies
+                if(nbCoups < getCookie(BestScoreCookie)){
+                    //On a battu le meilleur score !
+                    setCookie(BestScoreCookie, nbCoups);
+                    BestScoreNode.innerText = nbCoups;
+                }
             }
         }
     }
@@ -100,6 +116,8 @@ function initGame(nbPaires){
     gameBoard.innerHTML = "";
     nbPairesOnGame = nbPaires;
     cptCartesTrouvees = 0;
+    nbCoups = 0;
+    nbCoupsCurrentNode.innerText = nbCoups;
     let gameCard = [];
     for (let i = 0; i < nbPaires; i++) {
         gameCard.push([cards[i], false]);
@@ -140,7 +158,7 @@ function getRandomArbitrary(min, max) {
 
 function getHtmlCodeCard(nomCard, id){
     return `<div class="card hidden" id="${id}" data-image="${nomCard}">
-                <img src="/img/${nomCard}.png" />
+                <img src="img/${nomCard}.png" />
             </div>`;
 }
 
@@ -163,3 +181,34 @@ function stopAnimation(){
     animateDiv.innerHTML = "";
 }
 
+function setCookie(name, value) {
+    // Encode value in order to escape semicolons, commas, and whitespace
+    var cookie = name + "=" + encodeURIComponent(value);
+    
+    
+        /* Sets the max-age attribute so that the cookie expires
+        after the specified number of days */
+        cookie += "; max-age=" + (100*24*60*60);
+        
+        document.cookie = cookie;
+}
+
+function getCookie(name) {
+    // Split cookie string and get all individual name=value pairs in an array
+    var cookieArr = document.cookie.split(";");
+    
+    // Loop through the array elements
+    for(var i = 0; i < cookieArr.length; i++) {
+        var cookiePair = cookieArr[i].split("=");
+        
+        /* Removing whitespace at the beginning of the cookie name
+        and compare it with the given string */
+        if(name == cookiePair[0].trim()) {
+            // Decode the cookie value and return
+            return decodeURIComponent(cookiePair[1]);
+        }
+    }
+    
+    // Return null if not found
+    return null;
+}
